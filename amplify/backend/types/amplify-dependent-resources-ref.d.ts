2d420d973dc613fd8ca24b23473eb84e16c05dcc
Brady Georgen <brady@familiarcat.com>
2024-01-18 16:55:24 -0600

    added amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,38 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "resumebuilder": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "resumebuilder": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "openAI": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "resumebuilderlayer69ee0665": {
+      "Arn": "string"
+    }
+  },
+  "storage": {
+    "s3resumebuilderstorage1a319798": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
