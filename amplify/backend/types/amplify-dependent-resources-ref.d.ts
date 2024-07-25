ce8cdebdc13a407b729f13867f35c903d729a825
Brady Georgen <brady@familiarcat.com>
2024-07-25 13:56:56 -0500

    moving more app ids

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,35 +1,35 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    resumebuilder: {
-      GraphQLAPIEndpointOutput: "string"
-      GraphQLAPIIdOutput: "string"
-      GraphQLAPIKeyOutput: "string"
+  "api": {
+    "resumebuilder": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
-  }
-  auth: {
-    resumebuilder: {
-      AppClientID: "string"
-      AppClientIDWeb: "string"
-      IdentityPoolId: "string"
-      IdentityPoolName: "string"
-      UserPoolArn: "string"
-      UserPoolId: "string"
-      UserPoolName: "string"
+  },
+  "auth": {
+    "resumebuilder": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
-  }
-  function: {
-    openAI: {
-      Arn: "string"
-      LambdaExecutionRole: "string"
-      LambdaExecutionRoleArn: "string"
-      Name: "string"
-      Region: "string"
+  },
+  "function": {
+    "openAI": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
-  }
-  storage: {
-    s3resumebuilderstorage1a319798: {
-      BucketName: "string"
-      Region: "string"
+  },
+  "storage": {
+    "s3resumebuilderstorage1a319798": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
