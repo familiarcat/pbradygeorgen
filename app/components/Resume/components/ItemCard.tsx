f43d2e197511b53e1668c78698c6c0657e41a525
Brady Georgen <brady@familiarcat.com>
2024-08-28 13:54:01 -0500

    committing before refactoring ItemCard

--- app/components/Resume/components/ItemCard.tsx
+++ app/components/Resume/components/ItemCard.tsx
@@ -75,7 +75,7 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     padding: 16,
     margin: 8, // Space between cards
-    backgroundColor: "rgba(255,255,0,1)",
+    backgroundColor: "rgba(255,255,255,1)",
     borderRadius: 8,
     flexGrow: 1,
 
