e35407b638215db70f9503a8c11f50f88eb7b11e
Brady Georgen <pbradygeorgen@me.com>
2025-04-24 10:12:37 -0500

    Update Node.js version to 20 to support pdfjs-dist@5.1.91 requirement

--- amplify-prebuild.sh
+++ amplify-prebuild.sh
@@ -9,11 +9,11 @@ echo "Initial NPM version: $(npm -v)"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install Node.js 18 if not already installed
-nvm install 18
+# Install Node.js 20 if not already installed
+nvm install 20
 
-# Use Node.js 18
-nvm use 18
+# Use Node.js 20
+nvm use 20
 
 # Print updated Node.js and npm versions
 echo "Updated Node version: $(node -v)"
