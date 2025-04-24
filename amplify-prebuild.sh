bfbd3deff4f7be3bf544be490663b57ce87e4928
Brady Georgen <pbradygeorgen@me.com>
2025-04-24 10:37:19 -0500

    Update build configuration to be compatible with Node.js 14

--- amplify-prebuild.sh
+++ amplify-prebuild.sh
@@ -5,19 +5,10 @@
 echo "Initial Node version: $(node -v)"
 echo "Initial NPM version: $(npm -v)"
 
-# Setup NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Install Node.js 20 if not already installed
-nvm install 20
-
-# Use Node.js 20
-nvm use 20
-
-# Print updated Node.js and npm versions
-echo "Updated Node version: $(node -v)"
-echo "Updated NPM version: $(npm -v)"
+# Create .npmrc file to ignore engine requirements
+echo "engine-strict=false" >.npmrc
+echo "ignore-engines=true" >>.npmrc
+cat .npmrc
 
 # Exit with success
 exit 0
